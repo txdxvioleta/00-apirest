6143fb8c891f32ada1c7de1709cb82b300806a3a
Gonzalo Caram <gcaram95@gmail.com>
2022-09-22 04:10:28 -0300

    Implementación de validaciones y refactorización de controllers.js

--- src/controllers/controllers.js
+++ src/controllers/controllers.js
@@ -1,70 +1,46 @@
 //* imports:
 const connection = require('../database/dbConfig');
 
-/**************************************************************/
-
 // GET:
-const getUsers = (req, res) => {
-  const sql_query = 'SELECT * FROM users';
-
-  connection.query(sql_query, (error, result) => {
-    //Verifico si existe algún error:
-    if (error) throw error;
-
-    //Si existen registros los muestro:
-    if (result.length > 0) {
-      res.json(result);
-      return;
-    }
-    res.send('No results');
+const getUsers = async (req, res) => {
+  await connection.query('SELECT * FROM users', (error, result) => {
+    !error && result.length > 0
+      ? res.json(result)
+      : res.status(500).json({ message: 'There are no users yet' });
   });
 };
 
 // GET id:
-const getUserById = (req, res) => {
-  const id = req.params.id;
-  const sql_query = `SELECT * FROM users WHERE id = ${id}`;
-  connection.query(sql_query, (error, result) => {
-    //Verifico si hay errores:
-    if (error) throw error;
-
-    //Si hay un resultado lo muestro:
-    if (result.length > 0) {
-      res.json(result);
-    } else {
-      res.send('User not found');
-      return;
-    }
+const getUserById = async (req, res) => {
+  await connection.query(`SELECT * FROM users WHERE id = ${req.params.id}`, (error, result) => {
+    !error && result.length > 0
+      ? res.status(200).json(result)
+      : res.status(404).json({ message: 'User not found' });
   });
 };
 
 // POST:
-const addUser = (req, res) => {
-  const sql_query = 'INSERT INTO users SET ?';
-
-  connection.query(sql_query, [req.body], (error) => {
-    if (error) throw error;
-    res.send('User added');
+const addUser = async (req, res) => {
+  await connection.query('INSERT INTO users SET ?', [req.body], (error) => {
+    !error ? res.send('User added') : res.status(500).json({ message: 'Something goes wrong' });
   });
 };
 
 // PUT:
-const updateUser = (req, res) => {
-  const sql_query = 'UPDATE users SET ? WHERE id = ?';
-
-  connection.query(sql_query, [req.body, req.params.id], (error) => {
-    if (error) throw error;
-    res.send('User updated');
+const updateUser = async (req, res) => {
+  await connection.query('UPDATE users SET ? WHERE id = ?', [req.body, req.params.id], (error, result) => {
+    !error && result.affectedRows > 0
+      ? res.status(200).json('User updated')
+      : res.status(404).json('User not found');
   });
 };
 
 // DELETE id:
-const deleteUser = (req, res) => {
-  const sql_query = 'DELETE FROM users WHERE id = ?';
-
-  connection.query(sql_query, [req.params.id], (error) => {
-    if (error) throw error;
-    res.send('User deleted');
+const deleteUser = async (req, res) => {
+  await connection.query('DELETE FROM users WHERE id = ?', [req.params.id], (error, result) => {
+    !error && result.affectedRows > 0
+      ? res.status(200).json('User deleted')
+      : res.status(404).json('User not found');
   });
 };
 
